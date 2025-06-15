f68941674de8a491def3933e7a2dbb15c197afc2
oleg <ollie@ipro-software.com>
2025-06-15 15:52:41 +0200

    Task 2: full network + bastion EC2

--- variables.tf
+++ variables.tf
@@ -9,3 +9,22 @@ variable "github_repository" {
   type        = string
   default     = "laliev/rsschool-devops-course-tasks"
 }
+
+
+variable "instance_type" {
+  description = "EC2 instance type for the lab host"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "key_pair_name" {
+  description = "Name of the SSH key pair to use"
+  type        = string
+  default     = "rs-school-key"
+}
+
+variable "my_ip" {
+  description = "CIDR of your public IP for SSH access"
+  type        = string
+  default     = "0.0.0.0/0" # change later to your real IP/CIDR
+}
